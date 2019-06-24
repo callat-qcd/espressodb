07da03e62f97398cd50565ec762a9f808ca07a3f
Christopher Koerber <christopher@ckoerber.com>
2019-06-24 15:08:02 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# LatteDB
+
+Lattice QCD database interface using [Django](https://docs.djangoproject.com/en/2.2/intro/tutorial01/) as the content manager.
+
+## Description
+
+## Install
+Install via pip
+```bash
+pip install [--user] [-e] .
+```
+
+## Run
+Type the following command in your bash to initiate an interactive server
+
+```
+python manage.py runserver
+```
+
+You can access the interface in your browser.
+
+
+## Authors
+* cchang
+* ckoerber
+
+## Contribute
+To be discussed...
---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Setup file for LatteDB
+"""
+__author__ = "cchang, ckoerber"
+__version__ = "0.1.0"
+
+from os import path
+
+from setuptools import setup, find_packages
+
+CWD = path.abspath(path.dirname(__file__))
+
+with open(path.join(CWD, "README.md"), encoding="utf-8") as inp:
+    LONG_DESCRIPTION = inp.read()
+
+with open(path.join(CWD, "requirements.txt"), encoding="utf-8") as inp:
+    REQUIREMENTS = [el.strip() for el in inp.read().split(",")]
+
+setup(
+    name="lattedb",
+    version=__version__,
+    description=None,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url=None,
+    author=__author__,
+    author_email=None,
+    keywords=[],
+    packages=find_packages(exclude=["docs", "tests"]),
+    install_requires=REQUIREMENTS,
+)
