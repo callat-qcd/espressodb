cffcb3c52e224232d0d69e42245d369c67527aef
Christopher Koerber <christopher@ckoerber.com>
2019-06-26 09:50:54 -0700

    Placed admin page on index and changed branding

--- lattedb/settings.py
+++ lattedb/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = "lattedb.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "lattedb", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- lattedb/urls.py
+++ lattedb/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("base/", include("base.urls")),
     path("admin/", admin.site.urls),
+    path("", admin.site.urls),
 ]
