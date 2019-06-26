c223d6ebdede15239f9d55a2b6476fa387a0453b
Christopher Koerber <christopher@ckoerber.com>
2019-06-26 12:28:30 -0700

    Added logging to `startapp`

--- lattedb/django/main/settings.py
+++ lattedb/django/main/settings.py
@@ -122,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"level": "INFO", "class": "logging.StreamHandler"}},
+    "loggers": {
+        "main.commands": {"handlers": ["console"], "level": "DEBUG", "propagate": True}
+    },
+}
