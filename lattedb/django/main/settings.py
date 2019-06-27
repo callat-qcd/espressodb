6205ff80b768ddc91a3a7a5292d920afca68376c
Christopher Koerber <christopher@ckoerber.com>
2019-06-27 14:59:18 -0700

    Added first example plotting script using bokeh

--- lattedb/django/main/settings.py
+++ lattedb/django/main/settings.py
@@ -62,7 +62,10 @@ ROOT_URLCONF = "lattedb.django.main.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "lattedb", "templates")],
+        "DIRS": [
+            os.path.join(BASE_DIR, "lattedb", "templates"),
+            os.path.join(BASE_DIR, "correlators", "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
