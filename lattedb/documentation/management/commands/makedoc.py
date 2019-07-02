66144d0c300ef6bf4bbcd42cab17242f4c94ccf6
Christopher Koerber <christopher@ckoerber.com>
2019-07-02 14:52:19 -0700

    Added sphinx parser for ectracting docs

--- lattedb/documentation/management/commands/makedoc.py
+++ lattedb/documentation/management/commands/makedoc.py
@@ -4,6 +4,7 @@
 import os
 import logging
 
+from bs4 import BeautifulSoup
 from sphinx.application import Sphinx
 
 from django.core.management.base import BaseCommand
@@ -25,10 +26,25 @@ class Command(BaseCommand):
     doctreedir = os.path.join(BASE_DIR, "documentation", "source", "_build")
     builder = "html"
 
+    doc_dir = os.path.join(BASE_DIR, "documentation", "templates", "apps")
+
     def handle(self, *args, **options):
         """Installs app in `lattedb`
         """
-        app = Sphinx(
+        Sphinx(
             self.sourcedir, self.confdir, self.outputdir, self.doctreedir, self.builder
-        )
-        app.build()
+        ).build()
+
+        app_doc_dir = os.path.join(self.outputdir, "apps")
+        for doc_file in os.listdir(app_doc_dir):
+
+            if not doc_file.endswith(".html") or "index" in doc_file:
+                continue
+
+            with open(os.path.join(app_doc_dir, doc_file), "r") as fin:
+                soup = BeautifulSoup(fin.read(), "lxml")
+
+            html = soup.find("div", attrs={"role": "main"})
+
+            with open(os.path.join(self.doc_dir, doc_file), "w") as fout:
+                fout.write(html.prettify())
