db4f226859314e3e2914b7e6f6a3b447a5eae73b
Christopher Koerber <christopher@ckoerber.com>
2019-07-02 09:28:46 -0700

    Moved templates and static from config to base

--- lattedb/config/settings.py
+++ lattedb/config/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "main", "static"),
+    os.path.join(BASE_DIR, "base", "static"),
     os.path.join(BASE_DIR, "correlator", "static"),
 ]
 
