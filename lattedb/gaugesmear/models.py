cd7e783b3059286a4cea0d9b4a680a081a61aa03
Christopher Koerber <christopher@ckoerber.com>
2019-07-12 16:50:53 -0700

    Changed steps to flowsteps

--- lattedb/gaugesmear/models.py
+++ lattedb/gaugesmear/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 from lattedb.base.models import Base
 
+
 class GaugeSmear(Base):
     """ Base table for application"
     """
+
 
 class Unsmeared(GaugeSmear):
     """
@@ -14,19 +16,19 @@ class Unsmeared(GaugeSmear):
 class WilsonFlow(GaugeSmear):
     """
     """
+
     flowtime = models.DecimalField(
         max_digits=3,
         decimal_places=2,
         help_text="Decimal(3,2): Flow time in lattice units",
     )
-
-    steps = models.PositiveSmallIntegerField(
-        help_text="PositiveSmallInt: Number of steps"
+    flowstep = models.PositiveSmallIntegerField(
+        help_text="PositiveSmallInt: Number of diffusion steps"
     )
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["flowtime", "steps"], name="unique_gaugesmear_wilsonflow"
+                fields=["flowtime", "flowstep"], name="unique_gaugesmear_wilsonflow"
             )
         ]
