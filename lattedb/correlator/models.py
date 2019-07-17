4d344a2faa10595b7df0cbc6f20f8c15fdeaae8b
Christopher Koerber <christopher@ckoerber.com>
2019-07-16 17:32:29 -0700

    Write auto tag generation of DWFTuning and Meson2pt correlator based on properties

--- lattedb/correlator/models.py
+++ lattedb/correlator/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from lattedb.base.models import Base
 
@@ -33,6 +34,12 @@ class DWFTuning(Correlator):
                 name="unique_correlator_dwftuning",
             )
         ]
+
+    def clean(self):
+        """Sets tag of the correlator based on sink5.
+        """
+        if self.tag is None:
+            self.tag = "pseudo_pseudo" if self.sink5 else "midpoint_pseudo"
 
 
 class Meson2pt(Correlator):
@@ -71,6 +78,48 @@ class Meson2pt(Correlator):
                 name="unique_correlator_meson2pt",
             )
         ]
+
+    def clean(self):
+        """Sets tag of the correlator based on propagators, the gaugeconfig and source.
+
+        Operators are sorted by their `mval` key.
+        """
+        p0 = self.propagator0
+        p1 = self.propagator1
+        if p1 < p0:  # sorted by mval
+            self.propagator0 = p1
+            self.propagator1 = p0
+
+        if self.tag is None:
+            gc0 = self.propagator0.gaugeconfig.specialization  # pylint: disable=E1101
+            p0 = self.propagator0.specialization  # pylint: disable=E1101
+            gc1 = self.propagator1.gaugeconfig.specialization  # pylint: disable=E1101
+            p1 = self.propagator1.specialization  # pylint: disable=E1101
+
+            if gc0 != gc1:
+                raise ValidationError(
+                    "What are you smoking?"
+                    " Propagators not on the same gaugeconfig?!"
+                    f"\n{gc0} != {gc1}"
+                )
+
+            strangeness = self.source.strangeness  # pylint: disable=E1101
+            if abs(strangeness) == 0:  # pylint: disable=E1101
+                dtype = "pion"
+            elif abs(strangeness) == 1:  # pylint: disable=E1101
+                dtype = "kaon"
+            elif abs(strangeness) == 2:  # pylint: disable=E1101
+                dtype = "etas"
+            else:
+                raise ValidationError(
+                    f"Received strangeness {strangeness} meson."
+                    " Don't know how to deal with that..."
+                )
+
+            if p1 == p0:
+                self.tag = f"{p0.tag}_on_{gc0.tag}_{dtype}"
+            else:
+                self.tag = f"{p1.tag}{p0.tag}_on_{gc0.tag}_{dtype}"
 
 
 class Baryon4DSeq3pt(Correlator):
