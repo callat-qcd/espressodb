3d90e250538af2c0a95748e990949ce82655ad23
Jason Chang <pyo0220@gmail.com>
2019-07-19 00:38:13 -0700

    moved hadron columns to base

--- lattedb/hadron/models.py
+++ lattedb/hadron/models.py
@@ -12,20 +12,11 @@ class Hadron(Base):
         blank=True,
         help_text="(Optional) Text: Description of the interpolating operator",
     )
-
-
-class Meson(Hadron):
-    """
-    """
-
     hadronsmear = models.ForeignKey(
         "hadronsmear.HadronSmear",
         on_delete=models.CASCADE,
         related_name="+",
         help_text="ForeignKey pointing to operator smearing",
-    )
-    structure = models.TextField(
-        null=False, blank=False, help_text="Text: Dirac structure of the operator"
     )
     parity = models.DecimalField(
         max_digits=1,
@@ -61,7 +52,6 @@ class Meson(Hadron):
             models.UniqueConstraint(
                 fields=[
                     "hadronsmear",
-                    "structure",
                     "parity",
                     "spin_x2",
                     "spin_z_x2",
@@ -69,7 +59,23 @@ class Meson(Hadron):
                     "isospin_z_x2",
                     "strangeness",
                 ],
-                name="unique_hadron_meson",
+                name="unique_hadron",
+            )
+        ]
+
+
+class Meson(Hadron):
+    """
+    """
+
+    structure = models.TextField(
+        null=False, blank=False, help_text="Text: Dirac structure of the operator"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["hadron_ptr_id", "structure"], name="unique_hadron_meson"
             )
         ]
 
@@ -78,23 +84,10 @@ class Basak(Hadron):
     """
     """
 
-    hadronsmear = models.ForeignKey(
-        "hadronsmear.HadronSmear",
-        on_delete=models.CASCADE,
-        related_name="+",
-        help_text="ForeignKey pointing to operator smearing",
-    )
     irrep = models.TextField(
         null=False,
         blank=False,
         help_text="Text: Irreducible representations of O^D_h (octahedral group)",
-    )
-
-    parity = models.DecimalField(
-        max_digits=1,
-        decimal_places=0,
-        null=False,
-        help_text="Decimal(1,0): Parity of hadronic operator",
     )
     embedding = models.DecimalField(
         max_digits=1,
@@ -104,43 +97,10 @@ class Basak(Hadron):
         help_text="Decimal(1,0): k-th embedding of O^D_h irrep.",
     )
 
-    spin_x2 = models.PositiveSmallIntegerField(
-        null=False, help_text="Text: Total spin times two"
-    )
-
-    spin_z_x2 = models.SmallIntegerField(
-        null=False, help_text="Text: Spin in z-direction times two"
-    )
-
-    isospin_x2 = models.PositiveSmallIntegerField(
-        null=False, help_text="Text: Total isospin times two"
-    )
-
-    isospin_z_x2 = models.SmallIntegerField(
-        null=False, help_text="Text: Isospin in z-direction times two"
-    )
-
-    strangeness = models.DecimalField(
-        max_digits=1,
-        decimal_places=0,
-        null=False,
-        help_text="Decimal(1,0): Strangeness of hadronic operator",
-    )
-
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=[
-                    "hadronsmear",
-                    "irrep",
-                    "parity",
-                    "embedding",
-                    "spin_x2",
-                    "spin_z_x2",
-                    "isospin_x2",
-                    "isospin_z_x2",
-                    "strangeness",
-                ],
+                fields=["hadron_ptr_id", "irrep", "embedding"],
                 name="unique_hadron_basak",
             )
         ]
