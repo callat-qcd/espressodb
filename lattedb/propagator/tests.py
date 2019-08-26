1502febec0065705975901803d7dd9d0e18bf95e
Christopher Koerber <christopher@ckoerber.com>
2019-08-26 13:02:45 -0700

    Added propagator test

--- lattedb/propagator/tests.py
+++ lattedb/propagator/tests.py
@@ -1,3 +1,21 @@
+"""Tests test_get_or_create_from_paramerters method
+"""
+
 from django.test import TestCase
 
-# Create your tests here.
+from lattedb.base.tests import BaseTest
+
+from lattedb.propagator.models import OneToAll
+
+import lattedb.gaugeconfig.tests as gaugeconfig_tests
+import lattedb.fermionaction.tests as fermionaction_tests
+
+
+class PropagatorTestCase(TestCase, BaseTest):
+
+    cls = OneToAll
+    parameters = {"origin_x": 1, "origin_y": 2, "origin_z": 3, "origin_t": 10}
+    test_tree = {
+        "fermionaction": fermionaction_tests.FermionActionTestCase,
+        "gaugeconfig": gaugeconfig_tests.GaugeConfigTestCase,
+    }
