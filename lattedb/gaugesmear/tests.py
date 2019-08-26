984095c127f15cbf2504401ffe974481fadfa863
Christopher Koerber <christopher@ckoerber.com>
2019-08-26 11:22:03 -0700

    Generalizations of gaugesmear test

--- lattedb/gaugesmear/tests.py
+++ lattedb/gaugesmear/tests.py
@@ -10,26 +10,24 @@ from lattedb.gaugesmear.models import GaugeSmear
 LOGGER = logging.getLogger("base")
 
 
-class WilsonFlowTestCase(TestCase):
+class GaugeSmearTestCase(TestCase):
     """Tests for WilsonFlow gaguge smear table
     """
 
-    parameters = {"gaugesmear": {"flowtime": 1.0, "flowstep": 100}}
+    cls = WilsonFlow
+    parameters = {"flowtime": 1.0, "flowstep": 100}
 
     def test_get_or_create_from_parameters(self):
         """Tests get or create from parameters method
 
         Creates an instance and checks attributes.
         """
-        parameters = {}
-        for pars in self.parameters.values():
-            parameters.update(pars)
 
-        WilsonFlow.get_or_create_from_parameters(parameters)
+        WilsonFlow.get_or_create_from_parameters(self.parameters)
 
         gaugesmears = GaugeSmear.objects.all()
         for gaugesmear in gaugesmears:
             self.assertIsInstance(gaugesmear.specialization, WilsonFlow)
-            for key, val in parameters.items():
+            for key, val in self.parameters.items():
                 if key in WilsonFlow.__dict__:
                     self.assertEqual(val, getattr(gaugesmear.specialization, key))
