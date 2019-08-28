e938651e88ef8ed1a8bc9d1a36b6c44786d5b213
Christopher Koerber <christopher@ckoerber.com>
2019-08-27 17:40:48 -0700

    Moved inherited columns to children to ensure uniquness

--- lattedb/gaugeaction/models.py
+++ lattedb/gaugeaction/models.py
@@ -7,6 +7,11 @@ class GaugeAction(Base):
     """ Base table for application
     """
 
+
+class LuescherWeisz(GaugeAction):
+    """
+    """
+
     beta = models.DecimalField(
         max_digits=10,
         decimal_places=6,
@@ -19,17 +24,6 @@ class GaugeAction(Base):
         null=True,
         help_text="(Optional) Decimal(10,6): Lattice spacing in fermi",
     )
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["beta", "type"], name="unique_gaugeaction")
-        ]
-
-
-class LuescherWeisz(GaugeAction):
-    """
-    """
-
     u0 = models.DecimalField(
         max_digits=10,
         decimal_places=6,
@@ -40,7 +34,6 @@ class LuescherWeisz(GaugeAction):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["gaugeaction_ptr_id", "u0"],
-                name="unique_gaugeaction_luescherweisz",
+                fields=["beta", "u0"], name="unique_gaugeaction_luescherweisz"
             )
         ]
