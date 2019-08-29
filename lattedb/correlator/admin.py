b9538b456e2ffdecfb13fce378a924fe80b1a998
Christopher Koerber <christopher@ckoerber.com>
2019-08-28 19:00:08 -0700

    Added first list filters

--- lattedb/correlator/admin.py
+++ lattedb/correlator/admin.py
@@ -7,35 +7,81 @@ from lattedb.correlator.models import Meson2pt, Baryon2pt
 admin.site.register(Meson2pt, BaseAdmin)
 
 
+class EnsembleListFilter(admin.SimpleListFilter):
+    title = "ensemble"
+    parameter_name = "ensembles"
+
+    def lookups(self, request, model_admin):
+        tags = Baryon2pt.objects.values_list(
+            "propagator0__onetoall__gaugeconfig__nf211__short_tag"
+        ).distinct()
+        return [(key[0], key[0]) for key in tags]
+
+    def queryset(self, request, queryset):
+        return (
+            queryset.filter(
+                propagator0__onetoall__gaugeconfig__nf211__short_tag=self.value()
+            )
+            if self.value()
+            else queryset
+        )
+
+
 class Baryon2ptAdmin(admin.ModelAdmin):
     list_display = (
         "id",
-        "view_propagator0",
-        "view_propagator1",
-        "view_propagator2",
-        "view_source",
-        "view_sink",
+        "tag",
+        "config",
+        "ensemble",
+        "origin",
+        "origin_t",
+        "parity",
+        "spin_x2",
+        "spin_z_x2",
+        "isospin_x2",
+        "isospin_z_x2",
     )
+    list_filter = ("tag", EnsembleListFilter)
 
     @staticmethod
-    def view_propagator0(obj):
-        return str(obj.propagator0.specialization)
+    def config(obj):
+        return obj.propagator0.specialization.gaugeconfig.specialization.config
 
     @staticmethod
-    def view_propagator1(obj):
-        return str(obj.propagator1.specialization)
+    def origin(obj):
+        return "(%3d, %3d, %3d)" % (
+            obj.propagator0.specialization.origin_x,
+            obj.propagator0.specialization.origin_y,
+            obj.propagator0.specialization.origin_z,
+        )
 
     @staticmethod
-    def view_propagator2(obj):
-        return str(obj.propagator2.specialization)
+    def origin_t(obj):
+        return obj.propagator0.specialization.origin_t
 
     @staticmethod
-    def view_source(obj):
-        return str(obj.source.specialization)
+    def parity(obj):
+        return obj.sink.specialization.parity
 
     @staticmethod
-    def view_sink(obj):
-        return str(obj.sink.specialization)
+    def spin_x2(obj):
+        return obj.sink.specialization.spin_x2
+
+    @staticmethod
+    def spin_z_x2(obj):
+        return obj.sink.specialization.spin_z_x2
+
+    @staticmethod
+    def isospin_x2(obj):
+        return obj.sink.specialization.isospin_x2
+
+    @staticmethod
+    def isospin_z_x2(obj):
+        return obj.sink.specialization.isospin_z_x2
+
+    @staticmethod
+    def ensemble(obj):
+        return obj.propagator0.specialization.gaugeconfig.specialization.short_tag
 
 
 admin.site.register(Baryon2pt, Baryon2ptAdmin)
