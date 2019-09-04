76fb7e3046297aecd6ec6740b39f2b9cad925231
Christopher Koerber <christopher@ckoerber.com>
2019-09-03 18:18:45 -0700

    Removed old status and added new status in fh compare.
    Changes are not migrated and tested yet

--- lattedb/project_fhcompare/models/status.py
+++ lattedb/project_fhcompare/models/status.py
@@ -1,2 +1,15 @@
 from django.db import models
-from lattedb.base.models import Base
+
+from lattedb.correlator.models import Baryon2pt
+from lattedb.status.models import FileStatus
+
+
+class Correlator_Baryon2pt(FileStatus):
+    object = models.ForeignKey(
+        Baryon2pt,
+        on_delete=models.CASCADE,
+        help_text="ForeignKey: Baryon two point correlation function",
+    )
+    source_group = models.PositiveSmallIntegerField(
+        null=True, help_text="PositiveSmallInt: Index to the source group"
+    )
