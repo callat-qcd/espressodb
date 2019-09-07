ed26a2775b89938788cd12cb5df7fd514e8d0bd2
Christopher Koerber <christopher@ckoerber.com>
2019-09-06 17:34:18 -0700

    Allowed to get correlator status from ensemble

--- lattedb/status/models/correlator.py
+++ lattedb/status/models/correlator.py
@@ -21,3 +21,32 @@ class Baryon2pt(FileStatus):
                 fields=["barryon2pt", "home"], name="unique_baryon2pt_file_status"
             )
         ]
+
+    @classmethod
+    def get_from_ensemble(
+        cls,
+        ensemble: "Ensemble",
+        propagator: str = "propagator0",
+        propagator_type: str = "OneToAll",
+    ) -> "QuerySet(Baryon2pt)":
+        """Returns all correlators which are associated with the ensemble.
+
+        The association is given through the propagator relation.
+
+        **Arguments**
+            ensemble: Ensemble
+                The ensemble of gaugeconfigs
+
+            propagator: str = "propagator0"
+                The propagator of the correlator associated with the gagugeconfig.
+                For this correlator, can be one out of
+                `[propagator0, propagator1, propagator2]`, but all should be on the same
+                gaugeconfig anyway.
+
+            propagator_type: str = "OneToAll"
+                The type of the propagator e.g. "OneToAll".
+        """
+        baryon2pts = _Baryon2pt.get_from_ensemble(
+            ensemble, propagator=propagator, propagator_type=propagator_type
+        )
+        return cls.objects.filter(baryon2pt__in=baryon2pts)
