479fee28c679e732726e4662b2f5f6fdd0d9052a
Christopher Koerber <christopher@ckoerber.com>
2019-09-13 10:12:03 -0700

    Added more security checks for the website if DEBUG is False

--- lattedb/config/settings.py
+++ lattedb/config/settings.py
@@ -178,3 +178,14 @@ GRAPH_MODELS = {
     "output": "models.pdf",
     "exclude_columns": ["user", "id", "description", "last_modified", "misc", "type"],
 }
+
+if not DEBUG:
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_SSL_REDIRECT = True
+    X_FRAME_OPTIONS = "DENY"
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
