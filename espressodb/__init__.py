3b1757f36e26f62b7c2884d521cdb2b361262df7
Christopher Koerber <christopher@ckoerber.com>
2019-09-19 11:21:06 -0700

    Setup of espressodb without settings needed for autodoc

--- espressodb/__init__.py
+++ espressodb/__init__.py
@@ -1,12 +1,34 @@
 # pylint: disable=C0413
 """Prepares the usage of the espressodb app
 """
-import os
 from django import setup as _setup
+from django.conf import settings
 
 
 def init():
     """Initializes the django environment for lattedb
     """
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "espressodb.config.settings")
+    settings.configure(
+        DEBUG=True,
+        INSTALLED_APPS=[
+            "espressodb.base",
+            "espressodb.documentation",
+            "espressodb.management",
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+        ],
+        LOGGING={
+            "version": 1,
+            "disable_existing_loggers": False,
+            "handlers": {"console": {"level": "INFO", "class": "logging.StreamHandler"}},
+            "loggers": {
+                "espressodb": {
+                    "handlers": ["console"],
+                    "level": "DEBUG",
+                    "propagate": True,
+                }
+            },
+        },
+    )
+
     _setup()
