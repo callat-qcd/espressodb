08c7d838b3e5d62ebbb275a8df336f21906b236d
Christopher Koerber <christopher@ckoerber.com>
2019-09-29 13:55:01 -0700

    Adjusted startapp command to new layout

--- espressodb/management/management/commands/startapp.py
+++ espressodb/management/management/commands/startapp.py
@@ -5,10 +5,8 @@ import logging
 
 from django.core.management.commands.startapp import Command as StartAppCommand
 
-try:
-    from espressodb.management.utilities.settings import ROOT_DIR, PROJECT_NAME
-except ValueError:
-    pass
+from espressodb.management.utilities.settings import ROOT_DIR, PROJECT_NAME
+from espressodb.management.utilities.files import ESPRESSO_DB_ROOT
 
 LOGGER = logging.getLogger("espressodb")
 
@@ -22,19 +20,23 @@ class Command(StartAppCommand):
 
         .. note: Overwrites default directory
         """
-        app_name = options["name"]
-        options["directory"] = base_dir = os.path.join(ROOT_DIR, PROJECT_NAME, app_name)
-        os.makedirs(os.path.join(base_dir))
+        options["template"] = os.path.join(
+            ESPRESSO_DB_ROOT, "espressodb", "management", "templates", "app"
+        )
 
-        print(options)
+        app_name = options["name"]
+        options["directory"] = base_dir = os.path.join(ROOT_DIR, PROJECT_NAME)
+        options["project_name"] = PROJECT_NAME
+
         super().handle(**options)
         LOGGER.info(
             "App `%s` was successfully created. In order to install it", app_name
         )
-        LOGGER.info("1. Adjust the app (directory `%s`)", base_dir)
+        LOGGER.info("1. Adjust the app (directory `%s%s%s`)", base_dir, os.sep, app_name)
         LOGGER.info(
-            "2. Add `%s` to the `PROJECT_APPS` in `settings.yaml`",
-            f"espressodb.{app_name}",
+            "2. Add `%s.%s` to the `PROJECT_APPS` in `settings.yaml`",
+            PROJECT_NAME,
+            app_name,
         )
-        LOGGER.info("3. Run `espressodb makemigrations`")
-        LOGGER.info("4. Run `espressodb migrate`")
+        LOGGER.info("3. Run `python manage.py makemigrations`")
+        LOGGER.info("4. Run `python manage.py  migrate`")
