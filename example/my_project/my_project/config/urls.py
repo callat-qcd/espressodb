6655eec59ce3270f5494be14e36b9810dcbf29a3
Christopher Koerber <christopher@ckoerber.com>
2019-09-30 18:10:17 -0700

    Added bokeh status view

--- example/my_project/my_project/config/urls.py
+++ example/my_project/my_project/config/urls.py
@@ -13,8 +13,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.urls import path, include
 
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from espressodb.management.utilities.settings import PROJECT_APPS
+from espressodb.management.utilities.settings import ROOT_DIR
+
 urlpatterns = [
-    path("", include("espressodb.base.urls"))
+    path("", include("espressodb.base.urls", namespace="base")),
+    path("admin/", admin.site.urls),
+    path(
+        "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        r"documentation/",
+        include("espressodb.documentation.urls", namespace="documentation"),
+    ),
 ]
+
+for app in PROJECT_APPS:
+    url_file = os.path.join(ROOT_DIR, app.replace(".", "/"), "urls.py")
+    if os.path.exists(url_file):
+        _, app_name = app.split(".")
+        if app_name == "config":
+            continue
+        urlpatterns.append(
+            path(rf"{app_name}/", include(app + ".urls", namespace=app_name))
+        )
