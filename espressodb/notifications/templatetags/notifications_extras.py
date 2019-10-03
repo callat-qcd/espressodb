c8899f6b964d5e5591ce9018f23f8e8c4d50393e
Christopher Koerber <christopher@ckoerber.com>
2019-10-03 16:26:06 -0700

    Updated notification rendering

--- espressodb/notifications/templatetags/notifications_extras.py
+++ espressodb/notifications/templatetags/notifications_extras.py
@@ -14,3 +14,15 @@ def render_notification(notification: Notification):
     context = {"notification": notification}
 
     return context
+
+
+@register.simple_tag
+def bootstrap_level(level: str) -> str:
+    """Maps logging levels to bootstrap levels. Defaults to light.
+    """
+    return {
+        "DEBUG": "secondary",
+        "INFO": "info",
+        "WARNING": "warning",
+        "ERROR": "danger",
+    }.get(level, "light")
