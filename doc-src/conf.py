5b219a156a43fa448a2a1ddfabac2121874df628
Christopher Koerber <christopher@ckoerber.com>
2019-10-14 11:53:13 -0700

    Added notes on the relation tables
    * Added markdown table parser for sphinx

--- doc-src/conf.py
+++ doc-src/conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.autodoc",
     "sphinx_autodoc_typehints",
+    "sphinx_markdown_tables",
     # "sphinx.ext.viewcode",
     "recommonmark",
     "sphinx.ext.autosummary",
