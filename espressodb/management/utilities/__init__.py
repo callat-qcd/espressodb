566365e1c221a684621ce2055f629b52cee6bc5d
Christopher Koerber <christopher@ckoerber.com>
2019-10-17 14:53:25 -0700

    Related to prev commit

--- espressodb/management/utilities/__init__.py
+++ espressodb/management/utilities/__init__.py
@@ -1,9 +1,2 @@
 """The utilities submodule provides access to file or repo based EspressoDB information
-
-.. autosummary::
-   espressodb.management.utilities.files
-   espressodb.management.utilities.settings
-   espressodb.management.utilities.version
-
-------
 """
