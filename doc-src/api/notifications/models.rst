3abf8e2565a9f0fa76e23d8e0f8f11e3606fb560
Christopher Koerber <christopher@ckoerber.com>
2019-10-18 14:48:42 -0700

    Doc for notification models

--- doc-src/api/notifications/models.rst
+++ doc-src/api/notifications/models.rst
@@ -5,3 +5,4 @@ models
 .. automodule:: espressodb.notifications.models
     :members:
     :special-members:
+    :exclude-members: __weakref__
--- espressodb/notifications/models.py
+++ espressodb/notifications/models.py
@@ -1,5 +1,28 @@
 # pylint: disable=E1101
 """Implements notifications similar to logging with optional viewer restrictions
+
+The purpose of this module is having logging like capabilities which can be accessed
+in web views.
+Mimicing logging messages, notifications have a content field, a timestamp and a level.
+To have control over what might be displayed on web views, the notifications come with
+additional optional features:
+
+.. autosummary::
+    Notification.title
+    Notification.tag
+    Notification.groups
+    Notification.read_by
+
+The notifications view will be rendered on default whenever a user is logged in.
+
+------
+
+.. autosummary::
+    LEVELS
+    Notification
+    Notifier
+
+------
 """
 from typing import Optional
 from typing import List
@@ -8,34 +31,48 @@ from django.db import models
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import User
 
+#: The available notifications levels
 LEVELS = ("DEBUG", "INFO", "WARNING", "ERROR")
 
-# Create your models here.
+
 class Notification(models.Model):
-    """Model which implements logging like notification module
+    """Model which implements logging like notification interface.
+
+    The model is ordered according to `timestamp` in descending order.
     """
 
+    #: (:class:`models.CharField`) - The title of the notification
     title = models.CharField(
         max_length=200, help_text="The title of the notification", null=True, blank=True
     )
+    #: (:class:`models.TextField`) - The content of the notification
     content = models.TextField(help_text="The content of the notification")
+    #: (:class:`models.CharField`) -
+    #: The level of the notification mimicing logging levels. See also :data:`LEVELS`
     level = models.CharField(
         max_length=8,
         choices=[(level, level) for level in LEVELS],
         help_text="The level of the notification mimicing logging levels",
     )
+    #: (:class:`models.CharField`) - A tag for fast searches
     tag = models.CharField(
         max_length=100, null=True, help_text="A tag for fast searches", blank=True
     )
+    #: (:class:`models.ManyToManyField` -> :class:`django.contrib.auth.models.Group`) -
+    #: The group of users who are allowed to read this notification
     groups = models.ManyToManyField(
         Group,
         blank=True,
-        help_text="The group of users how are allowed to read this notification",
+        help_text="The group of users who are allowed to read this notification",
         related_name="notifications",
     )
+    #: (:class:`models.DateTimeField`) -
+    #: Creation date of the notification
     timestamp = models.DateTimeField(
         auto_now_add=True, help_text="Creation date of the notification"
     )
+    #: (:class:`models.ManyToManyField` -> :class:`django.contrib.auth.models.User`) -
+    #: The users who have read the notification
     read_by = models.ManyToManyField(
         User,
         blank=True,
@@ -47,22 +84,29 @@ class Notification(models.Model):
         ordering = ["-timestamp"]
 
     def add_user_to_read_by(self, user: User):
-        """Adds the user to the read_by list and inserts in the db.
+        """Adds the user to the :attr:`Notification.read_by` list and inserts in the db.
+
+        Arguments:
+            user: The user to check.
         """
         if not self.has_been_read_by(user):
             self.read_by.add(user)
             self.save()
 
-    def has_been_read_by(self, user: User):
+    def has_been_read_by(self, user: User) -> bool:
         """Checks if the user has read the notification
         """
         return self.read_by.filter(pk=user.pk).exists()  # pylint: disable=E1101
 
-    def viewable_by(self, user: Optional[User]):
+    def viewable_by(self, user: Optional[User]) -> bool:
         """Checks if the user is allowed to read this notification.
 
-        Returns False if the notification groups are not empty and user is not in the
-        specified groups.
+        Arguments:
+            user: The user to check.
+
+        Returns:
+            False if the notification groups are not empty and user is not in the
+            specified groups.
         """
         allowed_to_read = True
         if self.groups.exists():  # pylint: disable=E1101
@@ -101,70 +145,56 @@ class Notification(models.Model):
 
 
 class Notifier:
-    """Logger like object to set notifications
+    """Logger like object which interactions with the Notification model.
 
-    :ivar tag: The tag of the notification. Used for fast searches.
-    :ivar groups: The user groups which are allowed to view this notfication.
-        No groups means not logged in users are able to view the notfication.
-    :ivar fail_if_not_exists: If the user specifies group names which are not found,
-        the default behavior is that storing of notfications will fail
-        (e.g., raising KeyErrors).
-        If this is set to False, the code progress but logs Notifications to other
-        groups.
+    Example:
+        .. code::
 
-    .. admonition:: Example
+            notifier = Notifier(tag="my_app", groups=["admin"])
+            notifer.debug("Set up notifier")
+            notifier.info("Start to invesitgate app")
+            ...
+            notifier.error("ERROR! read this ...", title="NEED urgent attention!")
 
-        ```
-        notifier = Notifier(tag="my_app", groups=["admin"])
-        notifer.debug("Set up notifier")
-        notifier.info("Start to invesitgate app")
-        ...
-        notifier.error("Here is the error message ...", title="NEED urgent attention!")
-        ```
-
-    .. Note:
-
-        Note that `tag` and `groups` can be overwriten by the kwargs of the notifier
+        Note that ``tag`` and ``groups`` can be overwriten by the kwargs of the notifier
         methods, e.g., ``notifer.debug("Set up notifier", tag="set up")``
 
     """
 
     _keys = ("tag", "groups")
 
-    def __init__(
-        self,
-        tag: Optional[str] = None,
-        groups: Optional[List[str]] = None,
-        fail_if_not_exists: bool = True,
-    ):
+    def __init__(self, tag: Optional[str] = None, groups: Optional[List[str]] = None):
         """Init the Notifier class
 
-        :param tag: The tag of the notification. Used for fast searches.
-        :param groups: The user groups which are allowed to view this notfication.
-            No groups means not logged in users are able to view the notfication.
-        :param fail_if_not_exists: If the user specifies group names which are not found,
-            the default behavior is that storing of notfications will fail
-            (e.g., raising KeyErrors).
-            If this is set to False, the code progress but logs Notifications to other
-            groups.
-
+        Arguments:
+            tag:
+                The tag of the notification. Used for fast searches.
+            groups:
+                The user groups which are allowed to view this notfication.
+                No groups means not logged in users are able to view the notfication.
         """
+        #: The tag of the notification. Used for fast searches.
         self.tag = tag
+        #: The user groups which are allowed to view this notfication.
+        #: No groups means not logged in users are able to view the notfication.
         self.groups = self.get_groups_from_names(groups) if groups else []
-        self.fail_if_not_exists = fail_if_not_exists
 
-    def get_groups_from_names(self, group_names: List[str]) -> List[Group]:
-        """Parses the group names to actual groups
+    @staticmethod
+    def get_groups_from_names(group_names: List[str]) -> List[Group]:
+        """Parses the group names to :class:`Groups`.
 
-        :param group_names: List of group names which will be converted to a list of
+        Arguments:
+            group_names:
+                List of group names which will be converted to a list of
                 :class:`espressodb.notifications.models.Notification`.
 
-        Raises KeyError if `fail_if_not_exists` is True and not all groups are found.
-        Else returns found groups.
+        Raises:
+            KeyError:
+                If not all groups are found.
         """
         groups = Group.objects.filter(name__in=group_names)
 
-        if self.fail_if_not_exists and groups.count() != len(group_names):
+        if groups.count() != len(group_names):
             missing_groups = set(group_names).difference(
                 {groups.name for group in groups}
             )
@@ -183,13 +213,24 @@ class Notifier:
         tag: Optional[str] = None,
         groups: Optional[List[str]] = None,
     ) -> Notification:
-        """Creates a debug notification
+        """Creates notification at debug level.
 
-        :param content: The content of the notification
-        :param title: The title of the notification
-        :param tag: The tag of the notification. Used for fast searches.
-        :param groups: The user groups which are allowed to view this notfication.
-            No groups means not logged in users are able to view the notfication.
+        Arguments:
+            content:
+                The content of the notification
+            title:
+                The title of the notification
+            tag:
+                The tag of the notification. Used for fast searches.
+                Overrides Notifier default tag.
+            groups:
+                The user groups which are allowed to view this notfication.
+                No groups means not logged in users are able to view the notfication.
+                Overrides Notifier default groups.
+
+        Raises:
+            KeyError:
+                If groups are present but not found.
         """
         return self._create_notification(
             content=content, title=title, tag=tag, groups=groups, level="DEBUG"
@@ -202,13 +243,24 @@ class Notifier:
         tag: Optional[str] = None,
         groups: Optional[List[str]] = None,
     ) -> Notification:
-        """Creates an info notification
+        """Creates notification at info level.
 
-        :param content: The content of the notification
-        :param title: The title of the notification
-        :param tag: The tag of the notification. Used for fast searches.
-        :param groups: The user groups which are allowed to view this notfication.
-            No groups means not logged in users are able to view the notfication.
+        Arguments:
+            content:
+                The content of the notification
+            title:
+                The title of the notification
+            tag:
+                The tag of the notification. Used for fast searches.
+                Overrides Notifier default tag.
+            groups:
+                The user groups which are allowed to view this notfication.
+                No groups means not logged in users are able to view the notfication.
+                Overrides Notifier default groups.
+
+        Raises:
+            KeyError:
+                If groups are present but not found.
         """
         return self._create_notification(
             content=content, title=title, tag=tag, groups=groups, level="INFO"
@@ -221,13 +273,24 @@ class Notifier:
         tag: Optional[str] = None,
         groups: Optional[List[str]] = None,
     ) -> Notification:
-        """Creates a warning notification
+        """Creates notification at warning level.
 
-        :param content: The content of the notification
-        :param title: The title of the notification
-        :param tag: The tag of the notification. Used for fast searches.
-        :param groups: The user groups which are allowed to view this notfication.
-            No groups means not logged in users are able to view the notfication.
+        Arguments:
+            content:
+                The content of the notification
+            title:
+                The title of the notification
+            tag:
+                The tag of the notification. Used for fast searches.
+                Overrides Notifier default tag.
+            groups:
+                The user groups which are allowed to view this notfication.
+                No groups means not logged in users are able to view the notfication.
+                Overrides Notifier default groups.
+
+        Raises:
+            KeyError:
+                If groups are present but not found.
         """
         return self._create_notification(
             content=content, title=title, tag=tag, groups=groups, level="WARNING"
@@ -240,13 +303,24 @@ class Notifier:
         tag: Optional[str] = None,
         groups: Optional[List[str]] = None,
     ) -> Notification:
-        """Creates a error notification
+        """Creates notification at error level.
 
-        :param content: The content of the notification
-        :param title: The title of the notification
-        :param tag: The tag of the notification. Used for fast searches.
-        :param groups: The user groups which are allowed to view this notfication.
-            No groups means not logged in users are able to view the notfication.
+        Arguments:
+            content:
+                The content of the notification
+            title:
+                The title of the notification
+            tag:
+                The tag of the notification. Used for fast searches.
+                Overrides Notifier default tag.
+            groups:
+                The user groups which are allowed to view this notfication.
+                No groups means not logged in users are able to view the notfication.
+                Overrides Notifier default groups.
+
+        Raises:
+            KeyError:
+                If groups are present but not found.
         """
         return self._create_notification(
             content=content, title=title, tag=tag, groups=groups, level="ERROR"
@@ -255,13 +329,24 @@ class Notifier:
     def _create_notification(self, **kwargs) -> Notification:
         """Creates a notification entry in the db.
 
-        :param content: The content of the notification
-        :param title: The title of the notification.
-        :param tag: The tag of the notification. Used for fast searches.
-        :param level: The level of the notification.
-            Should be one of :py:const:`espressodb.notifications.models.LEVELS`.
-        :param groups: The user groups which are allowed to view this notfication.
-            No groups means not logged in users are able to view the notfication.
+        Arguments:
+            content:
+                The content of the notification
+            title:
+                The title of the notification
+            level:
+                The level of the notification.
+            tag:
+                The tag of the notification. Used for fast searches.
+                Overrides Notifier default tag.
+            groups:
+                The user groups which are allowed to view this notfication.
+                No groups means not logged in users are able to view the notfication.
+                Overrides Notifier default groups.
+
+        Raises:
+            KeyError:
+                If groups are present but not found.
         """
         options = kwargs.copy()
         options["tag"] = options.get("tag", None) or self.tag
