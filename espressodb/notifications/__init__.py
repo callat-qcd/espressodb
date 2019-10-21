7a3f5ab2a4a5645687cb2d1afef865c99506d305
Christopher Koerber <christopher@ckoerber.com>
2019-10-20 18:16:02 -0700

    EspressoDB notification bug fix
    Removed fail_if_not_exist from notifications init

--- espressodb/notifications/__init__.py
+++ espressodb/notifications/__init__.py
@@ -20,9 +20,7 @@ import espressodb
 
 
 def get_notifier(
-    tag: Optional[str] = None,
-    groups: Optional[List[str]] = None,
-    fail_if_not_exists: bool = True,
+    tag: Optional[str] = None, groups: Optional[List[str]] = None
 ) -> "espressodb.notifications.models.Notifier":
     """Get a notifier instance.
 
@@ -32,16 +30,10 @@ def get_notifier(
         groups:
             The user groups which are allowed to view this notfication.
             No groups means not logged in users are able to view the notfication.
-        fail_if_not_exists:
-            If the user specifies group names which are not found,
-            the default behavior is that storing of notfications will fail
-            (e.g., raising KeyErrors).
-            If this is set to False, the code progress but logs Notifications to other
-            groups.
 
     Returns:
         A notifier instance
     """
     from espressodb.notifications.models import Notifier
 
-    return Notifier(tag=tag, groups=groups, fail_if_not_exists=fail_if_not_exists)
+    return Notifier(tag=tag, groups=groups)
