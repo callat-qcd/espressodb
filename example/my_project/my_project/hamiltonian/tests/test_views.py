305ecf613b9ac6e98e09722b38a3a4497d537c4f
Christopher Koerber <christopher@ckoerber.com>
2019-11-20 15:38:52 -0800

    Added documentation unittest

--- example/my_project/my_project/hamiltonian/tests/test_views.py
+++ example/my_project/my_project/hamiltonian/tests/test_views.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 
 from bs4 import BeautifulSoup
 
+from espressodb.base.utilities.apps import get_project_apps
+
 from my_project.hamiltonian import models
 
 
@@ -22,6 +24,8 @@ class DocumentationViewTest(TestCase):
     """Tests the documentation view
     """
 
+    app_name = "my_project.hamiltonian"
+
     @property
     def content(self) -> BeautifulSoup:
         """Extracts the content of the view
@@ -29,21 +33,31 @@ class DocumentationViewTest(TestCase):
         response = self.client.get("/documentation/hamiltonian/")
         return BeautifulSoup(response.content, "html.parser")
 
-    def test_status_page(self):
+    def test_01_page_status(self):
         """Tests the HTTP status of the client
         """
         response = self.client.get("/documentation/hamiltonian/")
         self.assertEqual(response.status_code, 200)
 
-    def test_header(self):
+    def test_02_header(self):
         """Tests if the header of the doc page has the module name in doc string in it.
         """
         header = self.content.find("div", attrs={"class": "jumbotron"})
         self.assertIsNotNone(header)
         text = header.text
-        self.assertIn("my_project.hamiltonian", text)
+        self.assertIn(self.app_name, text)
         self.assertIn(models.__doc__, text)
 
-    def test_models(self):
+    def test_03_models(self):
+        """Tests if all model docs are rendered properly
         """
-        """
+        app = [app for app in get_project_apps() if app.name == self.app_name][0]
+        cards = self.content.find_all("div", attrs={"class": "card"})
+        self.assertEqual(len(cards), len(app.models))
+
+        for card in cards:
+            self.assertIn(card.attrs.get("id"), app.models)
+            model = app.models.get(card.attrs["id"])
+            fields = set(tr.find("td").text for tr in card.find("tbody").find_all("tr"))
+            expected_fields = {field.name for field in model.get_open_fields()}
+            self.assertEqual(fields, expected_fields)
