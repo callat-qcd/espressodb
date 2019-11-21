e05be40cf7bfc3149f86dbb31b4af1b561f6b9ea
Christopher Koerber <christopher@ckoerber.com>
2019-11-21 11:04:59 -0800

    First population view tests

---
+++ example/my_project/my_project/hamiltonian/tests/test_population_view.py
@@ -0,0 +1,60 @@
+"""Test for the population view
+"""
+from typing import Set
+
+from django.test import TestCase
+
+from bs4 import BeautifulSoup
+
+
+class PopulationViewTestCase(TestCase):
+    """Test for the population view.
+
+    Test clicks through the population page to generate a script which is
+    run and checked if the desired classes are generated.
+    """
+
+    @staticmethod
+    def get_soup(response) -> BeautifulSoup:
+        """Returns soup for response object
+        """
+        return BeautifulSoup(response.content, "html.parser")
+
+    def test_01_page_status(self):
+        """Tests if the page is found
+        """
+        response = self.client.get("/populate/")
+        self.assertEqual(response.status_code, 200)
+
+    def get_model_choices(self, soup) -> Set[str]:
+        """Locates form on page, selcts name select and extracts options.
+        """
+        form = soup.find("form")
+        self.assertIsNotNone(form)
+
+        select = form.find("select", attrs={"name": "model"})
+        self.assertIsNotNone(select)
+
+        return {el.text for el in select.find_all("option")}
+
+    def test_02_choices_present(self):
+        """Tests if the inital form has the right choices.
+        """
+        soup = self.get_soup(self.client.get("/populate/"))
+        self.assertEqual(
+            self.get_model_choices(soup),
+            {"Contact[Hamiltonian]", "Coulomb[Hamiltonian]", "Eigenvalue[Base]"},
+        )
+
+    def test_03_pick_eiegenvalue(self):
+        """Selects the eigenvalue option for the form
+        """
+        response = self.client.get("/populate/")  # needed to initialize cookies
+        response = self.client.post(
+            "/populate/",
+            data={"model": ["Eigenvalue[Base]"], "parse_tree": ["on"]},
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+
+        soup = self.get_soup(response)
