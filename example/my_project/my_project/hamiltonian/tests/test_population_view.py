199be14520a0d309dd52eb80eeb80f74ac313887
Christopher Koerber <christopher@ckoerber.com>
2019-11-21 11:11:18 -0800

    Population checks before generated script

--- example/my_project/my_project/hamiltonian/tests/test_population_view.py
+++ example/my_project/my_project/hamiltonian/tests/test_population_view.py
@@ -49,7 +49,7 @@ class PopulationViewTestCase(TestCase):
     def test_03_pick_eiegenvalue(self):
         """Selects the eigenvalue option for the form
         """
-        response = self.client.get("/populate/")  # needed to initialize cookies
+        self.test_01_page_status()  # needed to initialize cookies
         response = self.client.post(
             "/populate/",
             data={"model": ["Eigenvalue[Base]"], "parse_tree": ["on"]},
@@ -58,3 +58,32 @@ class PopulationViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
         soup = self.get_soup(response)
+        self.assertEqual(
+            self.get_model_choices(soup),
+            {"Contact[Hamiltonian]", "Coulomb[Hamiltonian]"},
+        )
+
+    def test_04_pick_hamiltonian(self) -> str:
+        """Selcts Hamiltonian after choosing an eigenvalue
+
+        Checks if the code block is present.
+
+        Returns:
+            The code block.
+        """
+        self.test_03_pick_eiegenvalue()  # needed to preselct on previous step
+        response = self.client.post(
+            "/populate/",
+            data={"model": ["Contact[Hamiltonian]"], "parse_tree": ["on"]},
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+
+        code = self.get_soup(response).find("pre", attrs={"id": "population-code"})
+        self.assertIsNotNone(code)
+
+        return code.text
+
+    def test_05_generated_script(self):
+        """Follows the call chain and tests the generated script
+        """
