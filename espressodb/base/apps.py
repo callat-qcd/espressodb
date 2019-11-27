89b2ed39d128e7125d6eb2602a9c9e0ff755bc84
Christopher Koerber <christopher@ckoerber.com>
2019-11-26 16:05:11 -0800

    Save and check consistency changes
    * Included pre save and pre add (m2m) signals
    * Moved check consistency to signals
    * Changed check_consitency to instance method
    * add check_m2m_consistency

--- espressodb/base/apps.py
+++ espressodb/base/apps.py
@@ -6,3 +6,8 @@ class BaseConfig(AppConfig):
     name = "espressodb.base"
     verbose_name = "EspressoDB Base"
     label = "base"
+
+    def ready(self):
+        """Loads signals from espressodb
+        """
+        import espressodb.base.signals  # pylint: disable=W0611
