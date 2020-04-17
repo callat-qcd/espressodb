callat-qcd/espressodb

commit 5d0510cf1fe73e7931fdb7a9ca719174f39752d8

Fixed anamyous typo in models save without user.
This closes #57. Thanks @kenmcelvain.

espressodb/base/models.py
@@ -45,14 +45,14 @@ class Base(models.Model):
         auto_now=True, help_text="Date the class was last modified"
     )
     #: User who updated this object. Set on save by connection to database.
-    #: Ananymous if not found.
+    #: Anonymous if not found.
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         help_text="User who updated this object. Set on save by connection to database."
-        " Ananymous if not found.",
+        " Anonymous if not found.",
     )
     #: User defined tag for easy searches
     tag = models.CharField(
@@ -223,7 +223,7 @@ class Base(models.Model):
             if username:
                 self.user, _ = User.objects.get_or_create(username=username)
             else:
-                self.user, _ = User.objects.get_or_create(username="ananymous")
+                self.user, _ = User.objects.get_or_create(username="anonymous")
 
         if self != self.specialization and not save_instance_only:
             self.specialization.save(*args, **kwargs)

commit 788ee25038db180e75bafcbd1a7f3af01fe88cd0

Migration unittest bugfixes
Correcting a typo in the user model in 
5d0510cf1fe73e7931fdb7a9ca719174f39752d8 accidentally broke the 
migration unit tests (since the docstring has changed)(you go test!). 
This fix also corrects the typo in the migration files

tests/migration_tests/migration_states/app1/migrations/0001_initial.py
@@ -15,16 +15,43 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='A1',
+            name="A1",
             fields=[
-                ('id', models.AutoField(help_text='Primary key for Base class.', primary_key=True, serialize=False)),
-                ('last_modified', models.DateTimeField(auto_now=True, help_text='Date the class was last modified')),
-                ('tag', models.CharField(blank=True, help_text='User defined tag for easy searches', max_length=200, null=True)),
-                ('i', models.IntegerField()),
-                ('user', models.ForeignKey(blank=True, help_text='User who updated this object. Set on save by connection to database. Ananymous if not found.', null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        help_text="Primary key for Base class.",
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                (
+                    "last_modified",
+                    models.DateTimeField(
+                        auto_now=True, help_text="Date the class was last modified"
+                    ),
+                ),
+                (
+                    "tag",
+                    models.CharField(
+                        blank=True,
+                        help_text="User defined tag for easy searches",
+                        max_length=200,
+                        null=True,
+                    ),
+                ),
+                ("i", models.IntegerField()),
+                (
+                    "user",
+                    models.ForeignKey(
+                        blank=True,
+                        help_text="User who updated this object. Set on save by connection to database. Anonymous if not found.",
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
-            options={
-                'abstract': False,
-            },
+            options={"abstract": False,},
         ),
     ]

tests/migration_tests/migration_states/app2/migrations/0001_initial.py
@@ -15,16 +15,43 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='A1',
+            name="A1",
             fields=[
-                ('id', models.AutoField(help_text='Primary key for Base class.', primary_key=True, serialize=False)),
-                ('last_modified', models.DateTimeField(auto_now=True, help_text='Date the class was last modified')),
-                ('tag', models.CharField(blank=True, help_text='User defined tag for easy searches', max_length=200, null=True)),
-                ('i', models.IntegerField()),
-                ('user', models.ForeignKey(blank=True, help_text='User who updated this object. Set on save by connection to database. Ananymous if not found.', null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        help_text="Primary key for Base class.",
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                (
+                    "last_modified",
+                    models.DateTimeField(
+                        auto_now=True, help_text="Date the class was last modified"
+                    ),
+                ),
+                (
+                    "tag",
+                    models.CharField(
+                        blank=True,
+                        help_text="User defined tag for easy searches",
+                        max_length=200,
+                        null=True,
+                    ),
+                ),
+                ("i", models.IntegerField()),
+                (
+                    "user",
+                    models.ForeignKey(
+                        blank=True,
+                        help_text="User who updated this object. Set on save by connection to database. Anonymous if not found.",
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
-            options={
-                'abstract': False,
-            },
+            options={"abstract": False,},
         ),
     ]

tests/migration_tests/migration_states/app3/migrations/0001_initial.py
@@ -15,16 +15,43 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='A1',
+            name="A1",
             fields=[
-                ('id', models.AutoField(help_text='Primary key for Base class.', primary_key=True, serialize=False)),
-                ('last_modified', models.DateTimeField(auto_now=True, help_text='Date the class was last modified')),
-                ('tag', models.CharField(blank=True, help_text='User defined tag for easy searches', max_length=200, null=True)),
-                ('i', models.IntegerField()),
-                ('user', models.ForeignKey(blank=True, help_text='User who updated this object. Set on save by connection to database. Ananymous if not found.', null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        help_text="Primary key for Base class.",
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                (
+                    "last_modified",
+                    models.DateTimeField(
+                        auto_now=True, help_text="Date the class was last modified"
+                    ),
+                ),
+                (
+                    "tag",
+                    models.CharField(
+                        blank=True,
+                        help_text="User defined tag for easy searches",
+                        max_length=200,
+                        null=True,
+                    ),
+                ),
+                ("i", models.IntegerField()),
+                (
+                    "user",
+                    models.ForeignKey(
+                        blank=True,
+                        help_text="User who updated this object. Set on save by connection to database. Anonymous if not found.",
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
-            options={
-                'abstract': False,
-            },
+            options={"abstract": False,},
         ),
     ]

tests/migration_tests/migration_states/app4/migrations/0001_initial.py
@@ -15,16 +15,43 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='A1',
+            name="A1",
             fields=[
-                ('id', models.AutoField(help_text='Primary key for Base class.', primary_key=True, serialize=False)),
-                ('last_modified', models.DateTimeField(auto_now=True, help_text='Date the class was last modified')),
-                ('tag', models.CharField(blank=True, help_text='User defined tag for easy searches', max_length=200, null=True)),
-                ('i', models.IntegerField()),
-                ('user', models.ForeignKey(blank=True, help_text='User who updated this object. Set on save by connection to database. Ananymous if not found.', null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        help_text="Primary key for Base class.",
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                (
+                    "last_modified",
+                    models.DateTimeField(
+                        auto_now=True, help_text="Date the class was last modified"
+                    ),
+                ),
+                (
+                    "tag",
+                    models.CharField(
+                        blank=True,
+                        help_text="User defined tag for easy searches",
+                        max_length=200,
+                        null=True,
+                    ),
+                ),
+                ("i", models.IntegerField()),
+                (
+                    "user",
+                    models.ForeignKey(
+                        blank=True,
+                        help_text="User who updated this object. Set on save by connection to database. Anonymous if not found.",
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
-            options={
-                'abstract': False,
-            },
+            options={"abstract": False,},
         ),
     ]