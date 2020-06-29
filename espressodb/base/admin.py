b2d10d76bdc72c39dbce9e05258d4f7c6ad3c668
ckoerber <software@ckoerber.com>
2020-06-29 12:16:53 +0200

    Moved ListViewAdmin init kwargs to static class member
    * This is more in accordance with Django's implementation and thus 
    allows to actually change them
    * Furthermore register_admin now takes the admin_class as input kwarg to 
    allow adjusting customizations in the default rendering process

--- espressodb/base/admin.py
+++ espressodb/base/admin.py
@@ -32,39 +32,38 @@ class BaseAdmin(admin.ModelAdmin):
 
 class ListViewAdmin(admin.ModelAdmin):
     """List view admin which displays all model fields.
+
+    Attributes:
+        search_fields:
+            The fields which are searchable on the admin page.
+            Does only render fields which are present in ``list_display``
+        list_display:
+            The fields to display as a column on the admin page.
+            Defaults to ``["id", "instance_name", ..., "tag"]`` where ``...`` are
+            the model default fields.
+        list_display_links:
+            The fields which will be a link to the detail view.
+            Defaults to ``["instance_name"]`` or ``["id"]`` or the first field in
+            ``list_display`` (if the previous option are not present in
+            ``list_display``).
+        **kwargs:
+            Kwargs for the parent init.
     """
 
+    search_fields: Tuple[str] = ("tag", "name")
+    list_display: Optional[List[str]] = None
+    list_display_links: Optional[List[str]] = None
+    display_instance_names: bool = True
+
     def __init__(  # pylint: disable=too-many-arguments
-        self,
-        model: Base,
-        admin_site: AdminSite,
-        search_fields: Tuple[str] = ("tag", "name"),
-        list_display: Optional[List[str]] = None,
-        list_display_links: Optional[List[str]] = None,
-        **kwargs,
+        self, model: Base, admin_site: AdminSite, **kwargs,
     ):
         """Sets the list display fields to espressodb defaults and model custom fields.
-
-        Arguments:
-            model:
-                The model to associate the admin site with.
-            admin_site:
-                The admin site.
-            search_fields:
-                The fields which are searchable on the admin page.
-                Does only render fields which are present in ``list_display``
-            list_display:
-                The fields to display as a column on the admin page.
-                Defaults to ``["id", "instance_name", ..., "tag"]`` where ``...`` are
-                the model default fields.
-            list_display_links:
-                The fields which will be a link to the detail view.
-                Defaults to ``["instance_name"]``
-            **kwargs:
-                Kwargs for the parent init.
         """
-        if list_display is None:
-            self.list_display = ["id", "instance_name"]
+        if self.list_display is None:
+            self.list_display = ["id"] + (
+                ["instance_name"] if self.display_instance_names else []
+            )
             if not issubclass(model, Base):
                 raise TypeError(
                     "To use the espressodb default list admin,"
@@ -78,17 +77,18 @@ class ListViewAdmin(admin.ModelAdmin):
             ]
             self.list_display += ["tag"]
 
-        else:
-            self.list_display = list_display
-
-        if list_display_links is None:
+        if self.list_display_links is None:
             if "instance_name" in self.list_display:
                 self.list_display_links = ["instance_name"]
+            elif "instance_name" in self.list_display:
+                self.list_display_links = ["id"]
+            else:
+                self.list_display_links = self.list_display[:1]
         else:
-            self.list_display_links = list_display_links
+            self.list_display_links = self.list_display_links
 
         self.search_fields = []
-        for search_field in search_fields:
+        for search_field in self.search_fields:
             if search_field in self.list_display:
                 self.search_fields.append(search_field)
 
@@ -104,13 +104,19 @@ class ListViewAdmin(admin.ModelAdmin):
         return str(obj)
 
 
-def register_admins(app_name: str, exclude_models: Optional[List[str]] = None):
+def register_admins(
+    app_name: str,
+    exclude_models: Optional[List[str]] = None,
+    admin_class: Optional[admin.ModelAdmin] = None,
+):
     """Tries to load all models from this app and registers :class:`ListViewAdmin` sites.
 
     Arguments:
         app_name: The name of the app.
         exclude_models: Models contained in this app which should not appear on the admin
             page. Uses the class name of the model.
+        admin_class: The admin model used to render the admin page.
+            Defaults to :class:`ListViewAdmin`.
 
     Calls :meth:`admin.site.register` for all models within the specified app.
     """
@@ -118,10 +124,12 @@ def register_admins(app_name: str, exclude_models: Optional[List[str]] = None):
 
     apps = [app for app in get_project_apps() if app.name == app_name]
 
+    admin_class = admin_class or ListViewAdmin
+
     if len(apps) == 1:
         for model in apps[0].get_models():
             if model.__name__ not in exclude_models:
-                admin.site.register(model, ListViewAdmin)
+                admin.site.register(model, admin_class)
     else:
         LOGGER.warning(
             "Was not able to locate app `%s`."
